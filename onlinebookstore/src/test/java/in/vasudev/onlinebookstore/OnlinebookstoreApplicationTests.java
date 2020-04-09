c67fb7275e2eb7adcf16d6b3d3395633aecc3297
Vasudev <vasudevburada07@gmail.com>
2020-04-09 16:52:28 +0530

    initial project strecture created

---
+++ onlinebookstore/src/test/java/in/vasudev/onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vasudev.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
